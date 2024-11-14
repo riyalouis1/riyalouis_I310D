20e13dacd18fa7d3b0c2482d90a7d3732e450642
riyalouis1 <riyalouis@utexas.edu>
2024-11-14 15:41:51 -0600

    volume_of_sphere.py

---
+++ volume_of_sphere.py
@@ -0,0 +1,12 @@
+def calculate_volume_of_sphere(radius):
+    pi = 3.14
+    volume = ((4/3) * pi * radius**3)
+    return volume
+
+radius = 30
+volume = calculate_volume_of_sphere(radius)
+print(volume)
+
+radius = 40
+volume = calculate_volume_of_sphere(radius)
+print(volume)
